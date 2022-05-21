e50f703f98403d612e86c9fd831a2813736bb6c5
Eugene Zherebnoy <eugene.zherebnoy@dataart.com>
2022-05-21 14:15:20 +0300

    add terraform

---
+++ terraform/dev/terraform.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+locals {
+  ssh_user         = "ubuntu"
+  key_name         = "ansible-da"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.dev-sg.id]
+  key_name                    = local.key_name
+  
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.web.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${aws_instance.web.public_ip} playbook.yaml"
+  }
+  depends_on = [aws_security_group.dev-sg]
+}
+
--- terraform/prod/terraform.tf
+++ terraform/prod/terraform.tf
@@ -28,15 +28,10 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  user_data = 
 
   tags = {
     Name = "HelloWorld"
