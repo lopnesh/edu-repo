5db6f89120edf772c27eb3a7974fdc715b8fdb37
Eugene Zherebnoy <eugene.zherebnoy@dataart.com>
2022-05-24 13:34:35 +0300

    terraform update

---
+++ terraform/dev/network.tf
@@ -0,0 +1,69 @@
+resource "aws_vpc" "dev-vpc" {
+  cidr_block           = "10.10.10.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  enable_classiclink   = false
+  instance_tenancy     = "default"
+
+  tags = {
+    "Name" = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "dev-subnet" {
+  vpc_id                  = aws_vpc.dev-vpc.id
+  cidr_block              = "10.10.10.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-central-1a"
+
+  tags = {
+    "Name" = "dev-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "dev-igw" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  tags = {
+    "Name" = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev-rt" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-igw.id
+  }
+
+  tags = {
+    Name = "dev-rt"
+  }
+}
+
+resource "aws_route_table_association" "dev-public-subnet-1" {
+  subnet_id      = aws_subnet.dev-subnet.id
+  route_table_id = aws_route_table.dev-rt.id
+}
+
+resource "aws_security_group" "dev-sg" {
+  name   = "dev-sg"
+  vpc_id = aws_vpc.dev-vpc.id
+
+dynamic "ingress" {
+    for_each = ["22", "3000"]
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
