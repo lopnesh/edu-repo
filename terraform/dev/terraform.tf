b78766e2405d55cdc09517497f40a4f523c1a0ab
Eugene Zherebnoy <eugene.zherebnoy@dataart.com>
2022-05-25 17:07:35 +0300

    Add packer

--- terraform/dev/terraform.tf
+++ terraform/dev/terraform.tf
@@ -8,17 +8,7 @@ locals {
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+  owners = ["086913749727"]
 }
 
 resource "aws_instance" "web" {
