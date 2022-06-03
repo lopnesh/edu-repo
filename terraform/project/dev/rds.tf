9d8c5fc207ba5b4d6db7dd5735c99e79ec345afd
Eugene Zherebnoy <eugene.zherebnoy@dataart.com>
2022-06-03 11:38:58 +0300

    rds

---
+++ terraform/project/dev/rds.tf
@@ -0,0 +1,19 @@
+resource "aws_db_instance" "mydb" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "blog"
+  username             = "user "
+  password             = "p@ssw0rd"
+  db_subnet_group_name = "my_database_subnet_group"
+  parameter_group_name = "default.mysql5.7"
+}
+
+resource "aws_route53_record" "database" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "rds.lopnesh.tk"
+  type = "CNAME"
+  ttl = "300"
+  records = ["${aws_db_instance.mydb.address}"]
+}
