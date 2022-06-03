579863f37d6c517d8936304ea7f0a594984c0347
Eugene Zherebnoy <eugene.zherebnoy@dataart.com>
2022-06-03 15:14:01 +0300

    rds

--- terraform/project/dev/rds.tf
+++ terraform/project/dev/rds.tf
@@ -7,7 +7,6 @@ resource "aws_db_instance" "mydb" {
   username             = "user "
   password             = "p@ssw0rd"
   db_subnet_group_name = aws_db_subnet_group.default.id
-  parameter_group_name = "default.mysql5.7"
 }
 resource "aws_db_subnet_group" "default" {
   name           = "main"
