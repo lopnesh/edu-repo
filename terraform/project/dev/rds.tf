de011ffc6e8b9d8e9b8d4d04d6354257de1c055f
Eugene Zherebnoy <eugene.zherebnoy@dataart.com>
2022-06-03 13:33:18 +0300

    rds

--- terraform/project/dev/rds.tf
+++ terraform/project/dev/rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "mydb" {
 }
 resource "aws_db_subnet_group" "default" {
   name           = "main"
-  subnet_ids     = [module.dev-vpc.aws_subnet.public_subnets[0].id, module.dev-vpc.aws_subnet.public_subnets[1].id]
+  subnet_ids     = [module.vpc-dev.aws_subnet.public_subnets[0].id, module.vpc-dev.aws_subnet.public_subnets[1].id]
   
   tags = {
     Name = "My DB subnet group"
