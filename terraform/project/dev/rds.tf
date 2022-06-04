639cc9208c18765be950dbc9bd99e039c5ef6f24
Eugene Zherebnoy <eugene.zherebnoy@dataart.com>
2022-06-04 18:54:43 +0300

    rds

--- terraform/project/dev/rds.tf
+++ terraform/project/dev/rds.tf
@@ -7,6 +7,7 @@ resource "aws_db_instance" "mydb" {
   username             = "admin"
   password             = "Gfhjkm123"
   skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.dev-sg.id]
   db_subnet_group_name = aws_db_subnet_group.default.id
   publicly_accessible  = true
 }
